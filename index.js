e3ae42df684e628bb1838bb34c2cd0d3d1732e0a
Rasheed <abdulrasheed8123@gmail.com>
2022-04-07 22:04:30 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 
 const app = express();
-
+// console.log("hwlelhdkjvks")
 const connect = () =>{
     return mongoose.connect("mongodb://127.0.0.1:27017/unit4c2")
 }
